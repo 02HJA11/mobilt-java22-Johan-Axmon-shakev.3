1d7639b7f8464c0ff352f77544370d3d4fe8e179
Johan <hansjohanaxmon@gmail.com>
2023-09-01 12:01:21 +0200

    the

--- app/src/main/java/com/example/shakey/MainActivity.java
+++ app/src/main/java/com/example/shakey/MainActivity.java
@@ -9,11 +9,13 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,16 +28,18 @@ public class MainActivity extends AppCompatActivity {
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         TextView text = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.textView);
 
         ImageButton ib = findViewById(R.id.imageButton);
         ImageView iv = findViewById(R.id.imageView2);
-        float ryan = 100;
+
 
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction()
                 .add(R.id.fragmentContainerView, BlankFragment2.class, null)
                 .setReorderingAllowed(true)
                 .commit();
+        MediaPlayer sound = MediaPlayer.create(this, R.raw.drive);
 
 
         SensorEventListener listener = new SensorEventListener() {
@@ -47,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
                 int sensor1 = (int) sensor;
                 int sensor2 = (int) sensorSecond;
                 int sensor3 = (int) sensorThird;
+                int ryan = (int) iv.getAlpha();
+                text2.setText("" + ryan);
 
                 text.setText("Rounded values: " + "\n" + sensor1 + "\n" + sensor2 + "\n" + sensor3 + "\n" + "Real time values: " + "\n" + sensor + "\n" + sensorSecond + "\n" + sensorThird);
                 ib.setRotation(sensor);
@@ -56,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
                 String sensortotext = String.valueOf(sensor);
                 Log.d("sensorChange", sensortotext);
 
+                if (ryan <= -50 || ryan >= 50) {
+                    sound.start();
+                    text2.setText("trigger");
+                }
                 if(sensor1 != 0 && sensor2 != 0 && sensor3 != 00) {
 
 
